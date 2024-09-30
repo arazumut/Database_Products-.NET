2f6802440b3f2a19498ff36634883bd6cd8b73df
arazumut <arazumut349@gmail.com>
2024-09-30 18:16:47 +0300

    Add To ÜrünÇıkış_Veritabanı Codes

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class UrunCikisVeritabani
+{
+    // Global değişkenler
+    static string urunIsmi;
+    static decimal urunFiyati;
+    static int urunMiktari;
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ürünün ismini giriniz: ");
+        urunIsmi = Console.ReadLine();
+
+        Console.Write("Ürünün fiyatını giriniz: ");
+        urunFiyati = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Ürünün miktarını giriniz: ");
+        urunMiktari = Convert.ToInt32(Console.ReadLine());
+
+        decimal toplamFiyat = urunFiyati * urunMiktari;
+
+        Console.WriteLine("Ürün Bilgileri:");
+        Console.WriteLine("İsim: " + urunIsmi);
+        Console.WriteLine("Fiyat: " + urunFiyati);
+        Console.WriteLine("Miktar: " + urunMiktari);
+        Console.WriteLine("Toplam Fiyat: " + toplamFiyat);
+    }
+}
